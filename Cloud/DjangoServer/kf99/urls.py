0bb5875da3a38668653d62eea4fc300ac65ed9a9
yooonyoung <dbsydde@gmail.com>
2020-12-04 18:22:35 +0900

    20201204 cloud

--- Cloud/DjangoServer/kf99/urls.py
+++ Cloud/DjangoServer/kf99/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'kf99'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    # path('', views.IndexView.as_view(), name='index'),
+    path('', views.mask),
 ]
