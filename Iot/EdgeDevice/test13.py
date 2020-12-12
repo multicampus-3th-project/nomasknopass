22a920fcb37ff331f75e938827f10a77abe9b9e4
leeve1241 <multirkh@gmail.com>
2020-12-12 11:32:40 +0900

    developing..

---
+++ Iot/EdgeDevice/test13.py
@@ -0,0 +1,11 @@
+from smbus2 import SMBus
+from mlx90614 import MLX90614
+import time
+
+bus = SMBus(1)
+sensor = MLX90614(bus, address=0x5A)
+def measureTemp():
+    while True:
+        time.sleep(2)
+        print("Object Temperature : {0:.2f}".format(sensor.get_object_1()))
+bus.close()
---
+++ Iot/EdgeDevice/test14.py
@@ -0,0 +1,20 @@
+import spidev, time
+def analog_read(channel):
+    # 매개변수 (시작비트, 채널, 자릿수 맞춤 위치), 리턴값 : 아날로그 값
+    r = spi.xfer2([1, (0x08+channel)<<4, 0])
+    adc_out = ((r[1]&0x03)<<8) + r[2] # 수신 데이터 결합
+    return adc_out
+
+spi = spidev.SpiDev()
+spi.open(0,0) # (버스, 디바이스)
+spi.mode = 3
+spi.max_speed_hz = 1000000
+present = 0
+past = 0
+while True:
+    adc = analog_read(0)
+    voltage = adc*3.3/1023
+    present = voltage
+    time.sleep(0.5)
+    print("past Voltage = %.4fV, present Voltage = %.4fV" % (present, past))
+    past = present
